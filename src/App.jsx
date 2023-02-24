1c1ddc1b851fb7c37ea0a8bbceb197b00b4337d4
suhas-scrobits <suhaskhobragade.scrobits@gmail.com>
2023-02-24 23:44:02 +0530

    added home link in landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Login from "./pages/Login";
-import SignWithGoogle from "./pages/SignWithGoogle";
-import RouteConfig from "../routes/RouteConfig";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId="334811094063-g29vhtndnefg64on21b6gu96kf5uf11e.apps.googleusercontent.com">
+    <>
       <BrowserRouter>
         <RouteConfig />
       </BrowserRouter>
-    </GoogleOAuthProvider>
+    </>
   );
 }
 
