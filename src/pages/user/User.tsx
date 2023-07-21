f0f40ba8321004ca369e05d5ea8629b43531301f
suhas-scrobits <suhaskhobragade.scrobits@gmail.com>
2023-07-21 22:19:59 +0530

    test: test3

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,30 +1,24 @@
-
-
-import CrowLogo from "./../../assets/CrowLogo.svg";
+import logo from "./../../assets/logo.svg";
 import HeroImage from "./../../assets/HeroImage.png";
 import Login from "./../../components/login/Login.tsx";
-import "./../../styles/AdminPage.css"; // Import the CSS for the split-screen 
+import "./../../styles/AdminPage.css"; // Import the CSS for the split-screen
 import { Divider } from "antd";
 
-
 function User() {
-    const dividerStyle = {
-        color: "#74788D",
-        fontFamily: "Poppins",
-        fontSize: "12px",
-        fontStyle: "normal",
-        fontWeight: 400,
-        lineHeight: "normal",
-        
-        
-      };
-      const orStyle = {
-        color: "#74788D", // Replace with your desired color for the "OR" text
-        // Add other styles for "OR" text if needed
-      };
+  const dividerStyle = {
+    color: "#74788D",
+    fontFamily: "Poppins",
+    fontSize: "12px",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "normal",
+  };
+  const orStyle = {
+    color: "#74788D", // Replace with your desired color for the "OR" text
+    // Add other styles for "OR" text if needed
+  };
   return (
     <div className="split-screen-container">
-      
       <div className="left-half">
         <div className="left-top-text">
           <text className="text1">
@@ -39,16 +33,18 @@ function User() {
       </div>
       <div className="right-half">
         <div className="top-right">
-          <img src={CrowLogo} alt="Hero Image" />
+          <img src={logo} alt="Hero Image" />
 
           <text className="text3">Welcome !</text>
           <text className="text4">Please enter your credentials below</text>
         </div>
-        <Login showForgotPassword={true} redirect="User"/>
-        <Divider plain style={dividerStyle} ><span style={orStyle}>OR</span></Divider>
+        <Login showForgotPassword={true} redirect="User" />
+        <Divider plain style={dividerStyle}>
+          <span style={orStyle}>OR</span>
+        </Divider>
         <div className="bottom-right">
-            <text className="text5">Don't have an account?</text>
-            <a href="#">Sign Up</a>
+          <text className="text5">Don't have an account?</text>
+          <a href="#">Sign Up</a>
         </div>
       </div>
     </div>
